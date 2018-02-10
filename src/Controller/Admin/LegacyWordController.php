a86ae767ad1aa304285bdba2bb7107f695625272
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-11 00:54:54 +0100

    Prepared controllers for admin

---
+++ src/Controller/Admin/LegacyWordController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LegacyWordController extends AbstractController
+{
+
+}
---
+++ src/Controller/Admin/SymbolController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SymbolController extends AbstractController
+{
+
+}
