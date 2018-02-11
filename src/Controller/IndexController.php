e0e890a3b6c154a50f471b91c72bfe26d3eb7d7d
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-11 19:45:27 +0100

    Update

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,6 +13,6 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index/index.html.twig');
+        return $this->render('app/index/index.html.twig');
     }
 }
