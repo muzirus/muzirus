e629d280f16baa0e1355d72cdc42400a90934c80
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-11 21:47:53 +0100

    Update

---
+++ assets/js/admin.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $(function () {
+        $('.js-data-table').DataTable();
+
+        // select2
+        $('.select2').select2();
+
+        // jquery.are-you-sure
+        $('.js-are-you-sure').areYouSure();
+
+        //$('[data-toggle="popover"]').popover();
+        //$('[data-toggle="tooltip"]').tooltip();
+        $('body').tooltip({
+            selector: '[data-toggle="tooltip"]'
+        });
+    });
+})(jQuery);
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,25 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(false)
     .createSharedEntry('js/common', ['jquery'])
-    .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', [
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './assets/js/app.js'
+    ])
+    .addEntry('js/admin', [
+        './node_modules/datatables.net/js/jquery.dataTables.js',
+        './node_modules/datatables.net-bs/js/dataTables.bootstrap.js',
+        './node_modules/jquery-slimscroll/jquery.slimscroll.min.js',
+        './node_modules/select2/dist/js/select2.min.js',
+        './node_modules/admin-lte/dist/js/adminlte.min.js',
+        './node_modules/jquery.are-you-sure/jquery.are-you-sure.js',
+        './assets/js/admin.js'
+    ])
     .addStyleEntry('css/app', './assets/scss/app.scss')
-    .addEntry('js/admin', './assets/js/admin.js')
-    .addStyleEntry('css/admin', './assets/scss/admin.scss')
+    .addStyleEntry('css/admin', [
+        './node_modules/admin-lte/dist/css/AdminLTE.min.css',
+        './node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+        './assets/scss/admin.scss'
+    ])
 ;
 
 module.exports = Encore.getWebpackConfig();
