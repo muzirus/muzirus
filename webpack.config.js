4048dd809b2f068935f8414be7799e43702e841e
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-11 19:43:37 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 var Encore = require('@symfony/webpack-encore');
 
-Encore
-    // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+// the project directory where compiled assets will be stored
+Encore.setOutputPath('public/build/');
 
-    // uncomment to define the assets of the project
-    .addEntry('js/app', './assets/js/app.js')
-    .addStyleEntry('css/app', './assets/css/app.css')
+// the public path used by the web server to access the previous directory
+Encore.setPublicPath('/build');
+Encore.cleanupOutputBeforeBuild();
+Encore.enableSourceMaps(!Encore.isProduction());
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+// uncomment to create hashed filenames (e.g. app.abc123.css)
+//Encore.enableVersioning(Encore.isProduction());
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
-;
+Encore.addEntry('js/app', './assets/js/app.js');
+Encore.addStyleEntry('css/app', './assets/scss/app.scss');
+Encore.addEntry('js/admin', './assets/js/admin.js');
+Encore.addStyleEntry('css/admin', './assets/scss/admin.scss');
+
+// uncomment if you use Sass/SCSS files
+Encore.enableSassLoader();
+
+// uncomment for legacy applications that require $/jQuery as a global variable
+//Encore.autoProvidejQuery();
 
 module.exports = Encore.getWebpackConfig();
