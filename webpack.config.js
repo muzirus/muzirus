ca2365e5f74efe6d5b141f33ae0370af27ab7367
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-11 20:08:39 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
 var Encore = require('@symfony/webpack-encore');
 
-// the project directory where compiled assets will be stored
-Encore.setOutputPath('public/build/');
-
-// the public path used by the web server to access the previous directory
-Encore.setPublicPath('/build');
-Encore.cleanupOutputBeforeBuild();
-Encore.enableSourceMaps(!Encore.isProduction());
-
-// uncomment to create hashed filenames (e.g. app.abc123.css)
-//Encore.enableVersioning(Encore.isProduction());
-
-Encore.addEntry('js/app', './assets/js/app.js');
-Encore.addStyleEntry('css/app', './assets/scss/app.scss');
-Encore.addEntry('js/admin', './assets/js/admin.js');
-Encore.addStyleEntry('css/admin', './assets/scss/admin.scss');
-
-// uncomment if you use Sass/SCSS files
-Encore.enableSassLoader();
-
-// uncomment for legacy applications that require $/jQuery as a global variable
-//Encore.autoProvidejQuery();
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(false)
+    .createSharedEntry('js/common', ['jquery'])
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/scss/app.scss')
+    .addEntry('js/admin', './assets/js/admin.js')
+    .addStyleEntry('css/admin', './assets/scss/admin.scss')
+;
 
 module.exports = Encore.getWebpackConfig();
