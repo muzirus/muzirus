8aa2425c84f7d43dc3445fa59da89a28df85636f
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-24 19:38:56 +0100

    Add git info extension

---
+++ src/Twig/Extension/GitInfoExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Twig\Extension;
+
+use App\Service\GitInfoService;
+
+class GitInfoExtension extends \Twig_Extension
+{
+    /**
+     * @var GitInfoService
+     */
+    private $gitInfoService;
+
+    public function __construct(GitInfoService $gitInfoService)
+    {
+        $this->gitInfoService = $gitInfoService;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new \Twig_SimpleFunction('get_git_commit_date', function () {
+                return $this->gitInfoService->getDate();
+            }),
+            new \Twig_SimpleFunction('get_git_commit_hash', function () {
+                return $this->gitInfoService->getHash();
+            }),
+        ];
+    }
+}
