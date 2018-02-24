acbd014f699e8033710493ee104bc37d6d569a2a
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-02-24 15:43:49 +0100

    Update russian and czech word controllers and add views

--- src/Controller/Admin/CzechWordController.php
+++ src/Controller/Admin/CzechWordController.php
@@ -3,8 +3,111 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AbstractController;
+use App\Entity\CzechWord;
+use App\Facade\CzechWordFacade;
+use App\Form\Word\CzechWordForm;
+use App\Repository\CzechWordRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CzechWordController extends AbstractController
 {
+    /**
+     * @var CzechWordFacade
+     */
+    private $czechWordFacade;
 
+    public function __construct(CzechWordFacade $czechWordFacade)
+    {
+        $this->czechWordFacade = $czechWordFacade;
+    }
+
+    /**
+     * @Route("", name="admin.czech-word")
+     * @Method("GET")
+     */
+    public function index(CzechWordRepository $czechWordRepository): Response
+    {
+        return $this->render(
+            'admin/czech-word/index.html.twig',
+            [
+                'czechWords' => $czechWordRepository->getAll(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/add", name="admin.czech-word.add")
+     * @Method({"GET", "POST"})
+     */
+    public function add(Request $request): Response
+    {
+        $form = $this->createForm(CzechWordForm::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->czechWordFacade->createWord($form->getData());
+
+            $this->addFlashSuccess('czech-word.created_successfully');
+
+            return $this->redirectToRoute('admin.czech-word');
+        }
+
+        return $this->render(
+            'admin/czech-word/add.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="admin.czech-word.view", requirements={"id": "\d+"})
+     * @Method("GET")
+     */
+    public function view(CzechWord $czechWord): Response
+    {
+        return $this->render(
+            'admin/czech-word/view.html.twig',
+            [
+                'czechWord' => $czechWord,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin.czech-word.edit", requirements={"id": "\d+"})
+     * @Method({"GET", "POST"})
+     */
+    public function edit(CzechWord $czechWord): Response
+    {
+        $form = $this->createForm(CzechWordForm::class);
+
+        // todo
+
+        return $this->render(
+            'admin/czech-word/edit.html.twig',
+            [
+                'czechWord' => $czechWord,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}/remove", name="admin.czech-word.remove", requirements={"id": "\d+"})
+     * @Method("POST")
+     */
+    public function remove(CzechWord $czechWord): RedirectResponse
+    {
+        $this->czechWordFacade->deleteWord($czechWord);
+
+        $this->addFlashSuccess('czech-word.deleted_successfully');
+
+        return $this->redirectToRoute('admin.czech-word');
+    }
 }
--- src/Controller/Admin/RussianWordController.php
+++ src/Controller/Admin/RussianWordController.php
@@ -3,8 +3,114 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AbstractController;
+use App\Entity\RussianWord;
+use App\Facade\RussianWordFacade;
+use App\Form\Word\RussianWordForm;
+use App\Repository\RussianWordRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("admin/russian-word")
+ */
 class RussianWordController extends AbstractController
 {
+    /**
+     * @var RussianWordFacade
+     */
+    private $russianWordFacade;
 
+    public function __construct(RussianWordFacade $russianWordFacade)
+    {
+        $this->russianWordFacade = $russianWordFacade;
+    }
+
+    /**
+     * @Route("", name="admin.russian-word")
+     * @Method("GET")
+     */
+    public function index(RussianWordRepository $russianWordRepository): Response
+    {
+        return $this->render(
+            'admin/russian-word/index.html.twig',
+            [
+                'russianWords' => $russianWordRepository->getAll(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/add", name="admin.russian-word.add")
+     * @Method({"GET", "POST"})
+     */
+    public function add(Request $request): Response
+    {
+        $form = $this->createForm(RussianWordForm::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->russianWordFacade->createWord($form->getData());
+
+            $this->addFlashSuccess('russian-word.created_successfully');
+
+            return $this->redirectToRoute('admin.russian-word');
+        }
+
+        return $this->render(
+            'admin/russian-word/add.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="admin.russian-word.view", requirements={"id": "\d+"})
+     * @Method("GET")
+     */
+    public function view(RussianWord $russianWord): Response
+    {
+        return $this->render(
+            'admin/russian-word/view.html.twig',
+            [
+                'russianWord' => $russianWord,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin.russian-word.edit", requirements={"id": "\d+"})
+     * @Method({"GET", "POST"})
+     */
+    public function edit(RussianWord $russianWord): Response
+    {
+        $form = $this->createForm(RussianWordForm::class);
+
+        // todo
+
+        return $this->render(
+            'admin/russian-word/edit.html.twig',
+            [
+                'russianWord' => $russianWord,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/{id}/remove", name="admin.russian-word.remove", requirements={"id": "\d+"})
+     * @Method("POST")
+     */
+    public function remove(RussianWord $russianWord): RedirectResponse
+    {
+        $this->russianWordFacade->deleteWord($russianWord);
+
+        $this->addFlashSuccess('russian-word.deleted_successfully');
+
+        return $this->redirectToRoute('admin.russian-word');
+    }
 }
