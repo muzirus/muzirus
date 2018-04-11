b3e0f9980d2b67a958e4dbffe219c913bba14254
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-04-11 21:56:50 +0200

    Remove unused code

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity()
  * @ORM\Table(name="users")
  * @ORM\HasLifecycleCallbacks()
  */
