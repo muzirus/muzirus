86269fea7d19678e9726c9f363b6ffc5a9a1d079
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-04-14 10:14:11 +0200

    Add simple users page in admin

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -3,7 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("admin/user")
+ */
 class UserController extends AbstractController
 {
+    /**
+     * @Route("", name="admin.user")
+     * @Method("GET")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render(
+            'admin/user/index.html.twig',
+            [
+                'users' => $userRepository->findAll(),
+            ]
+        );
+    }
 }
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Entity\UserInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method UserInterface[] findAll()
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
