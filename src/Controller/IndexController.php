d2dd44835dc601b92c02ad1298bfa7041fbc7a22
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-04-15 11:54:49 +0200

    Add language switcher

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="app.index")
+     * @Route("", name="app.index")
      */
     public function index(): Response
     {
         return $this->render('app/index/index.html.twig');
+    }
+
+    /**
+     * @Route("", name="app.index.without_locale")
+     */
+    public function indexWithoutLocale(): RedirectResponse
+    {
+        return $this->redirectToRoute('app.index');
     }
 }
---
+++ src/Twig/Extension/AppExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Twig\Extension;
+
+use Symfony\Component\Intl\Intl;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    private $localeCodes;
+
+    private $locales;
+
+    public function __construct($locales)
+    {
+        $this->localeCodes = explode('|', $locales);
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('get_locales', [$this, 'getLocales']),
+        ];
+    }
+
+    public function getLocales(): array
+    {
+        if (null !== $this->locales) {
+            return $this->locales;
+        }
+
+        $this->locales = [];
+        foreach ($this->localeCodes as $localeCode) {
+            $this->locales[] = [
+                'code' => $localeCode,
+                'name' => Intl::getLocaleBundle()->getLocaleName($localeCode, $localeCode),
+            ];
+        }
+
+        return $this->locales;
+    }
+}
