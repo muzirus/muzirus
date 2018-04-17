075867fb65dfbc64b20ca33b9e93dda037408ca7
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-04-17 21:49:53 +0200

    Load title page content from database

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    private const TITLE_PAGE_SLUG = 'http-www-muzirus-cz-cs';
+
     /**
      * @Route("", name="app.index")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('app/index/index.html.twig');
+        return $this->render(
+            'app/index/index.html.twig',
+            [
+                'page' => $postRepository->findOneBySlug(self::TITLE_PAGE_SLUG),
+            ]
+        );
     }
 
     /**
