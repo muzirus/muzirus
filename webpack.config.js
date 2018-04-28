261bcbab88984b1932300fe0c2157a48aa46a71d
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-04-28 03:15:42 +0200

    Fix AdminLTE

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ Encore
     .addEntry('js/app', './assets/js/app.js')
     .addEntry('js/admin', './assets/js/admin.js')
     .addStyleEntry('css/app', './assets/scss/app.scss')
-    .addStyleEntry('css/admin', './assets/scss/admin.scss')
+    .addStyleEntry('css/admin', [
+        './assets/scss/admin.scss',
+        './node_modules/admin-lte/dist/css/AdminLTE.min.css',
+        './node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+    ])
 ;
 
 module.exports = Encore.getWebpackConfig();
