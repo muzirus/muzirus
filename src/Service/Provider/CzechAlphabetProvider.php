14d023cf73777fb5c580365c6dd9a41483bdd192
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-05-08 15:01:00 +0200

    Create alphabet providers

---
+++ src/Service/Provider/CzechAlphabetProvider.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace App\Service\Provider;
+
+class CzechAlphabetProvider
+{
+    private const LETTERS = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'ch',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    ];
+
+    public static function getCzechLetters(): array
+    {
+        return self::LETTERS;
+    }
+
+    public static function getFirstCzechLetter(): string
+    {
+        return self::LETTERS[0];
+    }
+}
