e1a12f314120ef08042bbe5ed094d029d39fe837
Matus Petrofcik <petrofcikmatus@gmail.com>
2019-02-17 22:09:23 +0100

    Change import of AbstractMigration

--- src/Migrations/Version20180321213414.php
+++ src/Migrations/Version20180321213414.php
@@ -2,8 +2,8 @@
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 class Version20180321213414 extends AbstractMigration
 {
