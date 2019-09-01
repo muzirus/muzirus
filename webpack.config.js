f58864e09ffa43cf313b2c585cacfe9bfae8ca5a
Matus Petrofcik <petrofcikmatus@gmail.com>
2019-09-01 20:21:18 +0200

    Revise and update npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ Encore
     .enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(false)
+    .disableSingleRuntimeChunk()
     .addEntry('js/app', './assets/js/app.js')
     .addEntry('js/admin', './assets/js/admin.js')
     .addStyleEntry('css/app', './assets/scss/app.scss')
